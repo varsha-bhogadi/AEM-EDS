d25b6e8ade116f7c47f42d62cab2c780f63f1505
varsha-bhogadi <varshabhogadi@gmail.com>
2026-09-08 16:02:34 +0530

    Fix block JavaScript indentation

--- blocks/eds-enablement-block/eds-enablement-block.js
+++ blocks/eds-enablement-block/eds-enablement-block.js
@@ -1,9 +1,9 @@
 export default function decorate(block) {
-    block.classList.add('eds-enablement-block');
+  block.classList.add('eds-enablement-block');
 
-    const rows = [...block.children];
+  const rows = [...block.children];
 
-    rows.forEach((row) => {
-        row.classList.add('eds-enablement-row');
-    });
+  rows.forEach((row) => {
+    row.classList.add('eds-enablement-row');
+  });
 }
